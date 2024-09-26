276f512b5b660658d2826e586a724316119ee63f
pitch5623 <plbd.abou@gmail.com>
2024-09-26 11:48:32 +0100

    solution exo1

--- exo1/test_exo1.py
+++ exo1/test_exo1.py
@@ -7,5 +7,8 @@ class Exo1Test(unittest.TestCase):
 
     def test_item_construction(self):
         item = Item(10, 20)
-
+        self.assertEqual(item.price, 10)
         self.assertEqual(20, item.weight)
+        
+
+    
